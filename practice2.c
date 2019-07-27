54f9c16eee384449db3a396bfcd01cd328e3fecf
Shovon3198 <52400511+Shovon3198@users.noreply.github.com>
2019-07-27 11:17:07 +0600

    Add files via upload

---
+++ practice2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+   /*int a=7,b =3;
+   float result;
+
+   result = (float)a/b;
+
+   printf("Result = %f\n\n",result);
+
+   */
+
+   int a = 5,b=0;
+
+   double res = a/b;
+
+   printf("%lf",res);
+
+}
